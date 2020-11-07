cbcbb5533cd8efc0489f85656addfc6a57f7b78a
Bien Nguyen <bien.g.nguyen@gmail.com>
2020-11-07 02:04:53 -0600

    messing with icons

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 const About = () => {
   return (
     <Layout>
       <h1>This is the About page</h1>
+      <TwitterIcon />
     </Layout>
   );
 }
